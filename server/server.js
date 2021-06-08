b21a6df7fbffb25900d4a773fdaec0eed8fcde3c
DoganM95 <doganmermertas@hotmail.de>
2021-06-08 21:50:01 +0200

    fixed

--- server/server.js
+++ server/server.js
@@ -3,84 +3,72 @@ import {
 } from "module";
 const require = createRequire(
     import.meta.url);
+const escape = require('escape-html');
 const axios = require('axios');
 
-const credentials = require("../credentials/tasker_autoremote")
+const taskerAutoRemoteUrl = require("../credentials/tasker_autoremote")
+let productsToWatchJson = require("../configuration/products_to_watch.json")
 
 // urlToWatch = process.env.page_to_watch; //docker env var access for later
 
-let productsToWatch = [{
-        name: "AMD RX 6800 XT",
-        url: "https://amd.com/en/direct-buy/5458374100/de",
-        inStockString: "add to cart",
-        outOfStockString: "out of stock"
-    },
-    // {
-    //     name: "AMD RX 6800 XT MB",
-    //     url: "https://www.amd.com/de/direct-buy/5496921500/de"
-    // }
-]
+let productsToWatch = productsToWatchJson;
 
 // Adding flags to each product
 productsToWatch.map(product => {
-    product.inStock = true;
-    product.notified = false;
-    product.response = undefined;
+    product.lastNotifiedKeywordIndex = -1;
 });
 
-
+console.log("Watching the following products:");
 console.log(productsToWatch);
-console.log(credentials);
+console.log("Tasker AutoRemote destination: " + taskerAutoRemoteUrl);
 
 
-
+// Starts one stock watchdog for each product, independent of each other
 productsToWatch.forEach(async product => {
-
     while (true) {
-        product.notified = false; // Flag setting on each stock state switch
-
-        // Out of stock loop
-        while (product.inStock == false) {
-            await getStock(product, false);
-        }
-
-        // In stock loop
-        while (product.inStock == true) {
-            await getStock(product, true);
-        }
+        await getStock(product);
+        await sleep(product.checkInterval);
     }
 })
 
 async function getStock(product) {
+    console.log(new Date().getTime() + " - checking: " + product.name)
     // Get product status from website
     await axios.get(product.url)
         .then(res => {
-            if (res.status != 200)
-                throw new Error("HTTP response code: " + res.status)
-
-            if (String(res.data).toLowerCase().includes(product.inStockString)) {
-
-                if (!product.notified) {
-                    console.log(product.name + " in stock!!!");
-                    product.inStock = true;
-                    product.notified = true;
-                }
-
-            } else if (String(res.data).toLowerCase().includes(product.outOfStockString)) {
-
-                if (!product.notified) {
-                    console.log(product.name + " out of stock.");
-                    product.inStock = false;
-                    product.notified = true;
-                }
+            if (res.status != 200) {
+                console.log(rest.status + " status for " + product.name)
+                console.log("Waiting " + product.recoveryTime + " seconds for next request on product:")
+                console.log(product.name);
+                console.log("Current recovery time (until next request is made to undo a ban): " + (product.recoveryTime += 1000));
+                console.log("Increasing recovery time by 1 second on next failure, until the ban is gone");
             }
+            product.response = res;
         })
         .catch(err => {
+            console.error("Error on " + product.name);
             console.log(err);
-            // Wait 10 seconds after an error (recovery pause)
-            setTimeout(() => {}, 10000);
         })
 
-    // wait 0.5 seconds after each processed response
-    setTimeout(() => {}, 500);
+    try {
+        product.notificationKeywordPairs.forEach(async (keywordPair, keywordIndex) => {
+            if (String(product.response.data).toLowerCase().includes(keywordPair[1]) && product.lastNotifiedKeywordIndex != keywordIndex) {
+                product.lastNotifiedKeywordIndex = keywordIndex;
+                await action(product.name + " - " + keywordPair[0]);
+            }
+        })
+    } catch (err) {
+        console.log(err);
+    }
+
+}
+
+async function action(message) {
+    message = String(escape(message)).replaceAll(" ", "%20");
+    await axios.get(taskerAutoRemoteUrl.url + message);
+    console.log("sent message: " + message)
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
